1784ac75f28761cf4518f6b1e7f5e237ec221f26
Yumika Shiba <47481684+OrangeFrog210@users.noreply.github.com>
2019-07-17 16:54:45 -0600

    Add files via upload

--- PythonPrograms/gene_avg.py
+++ PythonPrograms/gene_avg.py
@@ -1,15 +1,17 @@
 """
 If multiple rows exist for a gene, then average each value in each column
 
-LM: July 11th, 2019
+LM: July 17th, 2019
 """
 import pandas as pd
-#import numpy as np
 
-fIn = "/home/yshiba/immu_melanoma/im_m_rnaseq_fb_intm.csv"
-fOut = "/home/yshiba/immu_melanoma/im_m_rnaseq_fb.csv"
+fIn = "/home/yshiba/immu_melanoma/im_m_rnaseq_fb_ntnorm_intm_.csv"
+fOut = "/home/yshiba/immu_melanoma/im_m_rnaseq_ntnorm_fb.csv"
 
 df = pd.read_csv(fIn, sep=',')
+
+print(df.dtypes)
+
 df_new = df.groupby(["geneName"]).mean()
 
 df_new.to_csv(fOut, sep='\t')
